2a13f0d95d485e053a31c58c94960987d7297fa4
MarrionTechDev <235007646+MarrionTechDev@users.noreply.github.com>
2026-08-27 21:42:40 -0400

    Add persistent task storage

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const tasks = [];
 
+
 const taskInput = document.querySelector("#taskInput");
 const form = document.querySelector("form");
 const taskList = document.querySelector("ul");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
-    const task = taskInput.value;
 
-    tasks.push(task);
+function loadTasks() {
+    const savedTasks = localStorage.getItem("tasks");
+
+    if (savedTasks){
+        const loadedTasks = JSON.parse(savedTasks);
+
+        tasks.push(...loadedTasks);
+    }
+}
+
+
+function renderTask(taskObject) {
 
     const li = document.createElement("li");
     li.classList.add("task");
@@ -18,15 +30,63 @@ form.addEventListener("submit", function(event) {
     checkbox.type = "checkbox";
     checkbox.classList.add("task-checkbox");
 
+    checkbox.checked = taskObject.completed;
+
+    checkbox.addEventListener("change", function() {
+        taskObject.completed = checkbox.checked;
+
+        saveTasks();
+    });
+
     const span = document.createElement("span");
-    span.textContent = task;
+    span.textContent = taskObject.name;
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener("click", function(event) {
+        const taskIndex = tasks.indexOf(taskObject)
+        
+        tasks.splice(taskIndex, 1);
+
+        saveTasks();
+
+        event.target.parentElement.remove();
+
+    });
 
     li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(deleteButton);
 
     taskList.appendChild(li);
+
+}
+
+loadTasks();
+
+tasks.forEach(function(task) {
+    renderTask(task);
+});
+
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const task = taskInput.value;
+
+    if (task === "") {
+        return;
+    }
+
+    const taskObject = {
+        name: task,
+        completed: false
+    };
+
+    tasks.push(taskObject);
+
+    saveTasks();
+
+    renderTask(taskObject);
 });
