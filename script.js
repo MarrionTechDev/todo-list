bd235c7238c356a6d8e199269f13ac7601a758e2
MarrionTechDev <235007646+MarrionTechDev@users.noreply.github.com>
2026-09-19 14:09:51 -0400

    Complete task reminders

--- script.js
+++ script.js
@@ -72,9 +72,9 @@ function renderTask(taskObject) {
     const span = document.createElement("span");
     span.textContent = taskObject.name;
 
-    // Scheduler
-    const scheduleButton = document.createElement("button");
-    scheduleButton.innerHTML = `
+    // Reminder
+    const reminderButton = document.createElement("button");
+    reminderButton.innerHTML = `
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="18"
@@ -93,25 +93,142 @@ function renderTask(taskObject) {
         </svg>
     `;
 
-    scheduleButton.addEventListener("click", function() {
+    function updateReminderButton() {
+        if (taskObject.reminder) {
+            reminderButton.innerHTML = `
+                 <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <path d="M21 12a9 9 0 1 1-2.64-6.36"></path>
+                    <polyline points="21 3 21 9 15 9"></polyline>
+                    <line x1="12" y1="7" x2="12" y2="12"></line>
+                    <line x1="12" y1="12" x2="15" y2="14"></line>
+                </svg>
+            `;
+        } else {
+            reminderButton.innerHTML = `
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <rect x="3" y="4" width="18" height="18" rx="2"></rect>
+                    <line x1="16" y1="2" x2="16" y2="6"></line>
+                    <line x1="8" y1="2" x2="8" y2="6"></line>
+                    <line x1="3" y1="10" x2="21" y2="10"></line>
+                </svg>
+            `;
+        }
+    }
 
-        const scheduleWindow = document.createElement("div");
-        scheduleWindow.classList.add("schedule-window");
-        document.body.appendChild(scheduleWindow);
+    updateReminderButton();
+
+    reminderButton.addEventListener("click", function() {
+
+        const reminderWindow = document.createElement("div");
+        reminderWindow.classList.add("reminder-window");
+        document.body.appendChild(reminderWindow);
 
         // Heading
         const heading = document.createElement("h1");
-        heading.textContent = "Schedule";
-        heading.classList.add("schedule-heading");
-        scheduleWindow.appendChild(heading);
+        heading.textContent = "Reminder";
+        heading.classList.add("reminder-heading");
+        reminderWindow.appendChild(heading);
+
+        // Close button
+        const closeButton = document.createElement("button");
+        closeButton.textContent = "×";
+        closeButton.classList.add("close-button");
+
+        reminderWindow.appendChild(closeButton);
+
+        closeButton.addEventListener("click", function() {
+            reminderWindow.remove();
+        });
+
+        if (taskObject.reminder) {
+
+            // Reminder info
+            const reminderInfo = document.createElement("div");
+            reminderInfo.classList.add("reminder-info");
+            reminderWindow.appendChild(reminderInfo);
+
+            const savedDate = new Date(taskObject.reminder.date);
+
+            const formattedDate = savedDate.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+                year: "numeric"
+            });
+
+            const [hours, minutes] = taskObject.reminder.time.split(":");
+
+            const timeDate = new Date();
+            timeDate.setHours(hours, minutes);
+
+            const formattedTime = timeDate.toLocaleTimeString("en-US", {
+                hour: "numeric",
+                minute: "2-digit"
+            });
+
+            const reminderLabel = document.createElement("p");
+            reminderLabel.textContent = "Reminder for:";
+            reminderLabel.classList.add("reminder-label");
+
+            const reminderDateText = document.createElement("p");
+            reminderDateText.textContent =
+                `${formattedDate} at ${formattedTime}`;
+            reminderDateText.classList.add("reminder-date");
+
+            // Create row
+            const reminderInfoRow = document.createElement("div");
+            reminderInfoRow.classList.add("reminder-info-row");
+
+            // Clear button
+            const clearReminderButton = document.createElement("button");
+            clearReminderButton.textContent = "Clear";
+            clearReminderButton.classList.add("clear-reminder-button");
+
+            clearReminderButton.addEventListener("click", function() {
+                taskObject.reminder = null;
+
+                updateReminderButton();
+
+                saveTasks();
+
+                reminderInfo.remove();
+            });
+ 
+            reminderInfoRow.appendChild(reminderDateText);
+            reminderInfoRow.appendChild(clearReminderButton);
+
+            reminderInfo.appendChild(reminderLabel);
+            reminderInfo.appendChild(reminderInfoRow);
+
+        }
         
         // Current month
-        const currentMonth = new Date(2026, 8, 1);
+        const currentMonth = new Date();
+        currentMonth.setDate(1);
 
         // Month navigation container
         const monthContainer = document.createElement("div");
         monthContainer.classList.add("month-container");
-        scheduleWindow.appendChild(monthContainer);
+        reminderWindow.appendChild(monthContainer);
         
         const monthNames = [
                 "January", "February", "March", "April", "May", "June", 
@@ -153,7 +270,7 @@ function renderTask(taskObject) {
         // Weekdays
         const weekdayContainer = document.createElement("div");
         weekdayContainer.classList.add("weekdays");
-        scheduleWindow.appendChild(weekdayContainer);
+        reminderWindow.appendChild(weekdayContainer);
 
         const weekdays = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
 
@@ -166,11 +283,25 @@ function renderTask(taskObject) {
         // Calendar
         const calendar = document.createElement("div");
         calendar.classList.add("calendar");
-        scheduleWindow.appendChild(calendar);
+        reminderWindow.appendChild(calendar);
 
-        let selectedDay = new Date().getDate();
-        let selectedMonth = new Date().getMonth();
-        let selectedYear = new Date().getFullYear();
+        let selectedDay;
+        let selectedMonth;
+        let selectedYear;
+
+        if (taskObject.reminder) {
+            const savedDate = new Date(taskObject.reminder.date);
+
+            selectedDay = savedDate.getDate();
+            selectedMonth = savedDate.getMonth();
+            selectedYear = savedDate.getFullYear();
+        } else {
+            const today = new Date();
+
+            selectedDay = today.getDate();
+            selectedMonth = today.getMonth();
+            selectedYear = today.getFullYear();
+        }
 
         function renderCalendar() {
             calendar.innerHTML = "";
@@ -231,19 +362,26 @@ function renderTask(taskObject) {
         const timeLabel = document.createElement("label");
         timeLabel.textContent = "Time:";
         timeLabel.classList.add("time-label");
-        scheduleWindow.appendChild(timeLabel);
+        reminderWindow.appendChild(timeLabel);
         
         const timeInput = document.createElement("input");
         timeInput.type = "time";
         timeInput.classList.add("time-input");
-        scheduleWindow.appendChild(timeInput);
+
+        if (taskObject.reminder) {
+            timeInput.value = taskObject.reminder.time;
+        }
+
+        reminderWindow.appendChild(timeInput);
+
+        
 
         // Cancel button
         const cancelButton = document.createElement("button");
         cancelButton.textContent = "Cancel";
 
         cancelButton.addEventListener("click", function() {
-                scheduleWindow.remove();
+                reminderWindow.remove();
         });
     
         // Save button
@@ -251,35 +389,52 @@ function renderTask(taskObject) {
         saveButton.textContent = "Save";
         saveButton.addEventListener("click", function() {
 
-            const scheduledDate = new Date(
+            const reminderDate = new Date(
                 selectedYear,
                 selectedMonth,
                 selectedDay
             );
 
-            taskObject.schedule = {
-                date: scheduledDate,
+            taskObject.reminder = {
+                date: reminderDate,
                 time: timeInput.value
             };
 
+            updateReminderButton();
             saveTasks();
-            scheduleWindow.remove();
+            reminderWindow.remove();
 
         });
 
         // Button Container
         const buttonContainer = document.createElement("div");
-        buttonContainer.classList.add("schedule-buttons");
+        buttonContainer.classList.add("reminder-buttons");
 
         buttonContainer.appendChild(cancelButton);
         buttonContainer.appendChild(saveButton);
 
-        scheduleWindow.appendChild(buttonContainer);
+        reminderWindow.appendChild(buttonContainer);
 
     });
 
     const editButton = document.createElement("button");
-    editButton.textContent = "Edit";
+    editButton.classList.add("edit-button");
+
+    editButton.innerHTML = `
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M4 20l4.5-1L19 8.5a2.12 2.12 0 0 0-3-3L5.5 16 4 20z"></path>
+        </svg>
+    `;
 
     let editInput;
     let editing = false;
@@ -293,40 +448,58 @@ function renderTask(taskObject) {
             const newName = editInput.value.trim();
 
             if (newName === ""){
+
                 taskObject.name = originalName;
-
                 span.textContent = originalName;
-
                 editInput.replaceWith(span);
-
-                editButton.textContent = "Edit";
-
+                editButton.innerHTML = `
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <path d="M4 20l4.5-1L19 8.5a2.12 2.12 0 0 0-3-3L5.5 16 4 20z"></path>
+                </svg>
+            `;
                 editing = false;
 
                 return;
             }
 
             taskObject.name = newName;
-
             span.textContent = taskObject.name;
-
             editInput.replaceWith(span);
-
-            editButton.textContent = "Edit";
-
+            editButton.innerHTML = `
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <path d="M4 20l4.5-1L19 8.5a2.12 2.12 0 0 0-3-3L5.5 16 4 20z"></path>
+                </svg>
+            `;
             editing = false;
 
             saveTasks();
 
         } else{
+
             editInput = document.createElement("input");
-
             editInput.classList.add("edit-input");
-        
             editInput.value = taskObject.name;
-
             span.replaceWith(editInput);
-
             editButton.textContent = "Save";
 
             editing = true;
@@ -371,10 +544,11 @@ function renderTask(taskObject) {
 
     li.appendChild(checkbox);
     li.appendChild(span);
-    li.appendChild(scheduleButton);
+    li.appendChild(reminderButton);
     li.appendChild(editButton);
     li.appendChild(deleteButton);
     
+
     taskList.appendChild(li);
 
 }
